4843e37960951f7fe863fe4c754b2534ec5c9546
GhanemYassine <46890231+GhanemYassine@users.noreply.github.com>
2021-01-03 12:17:49 +0100

    learning security

--- imports/ui/task.js
+++ imports/ui/task.js
@@ -2,6 +2,11 @@ import { Template } from "meteor/templating";
 import { Meteor } from "meteor/meteor";
 
 import "./task.html";
+Template.task.helpers({
+  isOwner() {
+    return this.owner === Meteor.userId();
+  },
+});
 
 Template.task.events({
   "click .toggle-checked"() {
@@ -10,5 +15,8 @@ Template.task.events({
   },
   "click .delete"() {
     Meteor.call("tasks.remove", this._id);
+  },
+  "click .toggle-private"() {
+    Meteor.call("tasks.setPrivate", this._id, !this.private);
   },
 });
